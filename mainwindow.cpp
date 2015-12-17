db700831f9513189d286ec60f7be1e84b016d9be
HamiguaLu <lugangxyz@hotmail.com>
2015-12-17 16:52:16 +0100

    Add settings handler

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,42 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QSettings>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    ui->m_lockTime->setValidator(new QIntValidator(1, 600, this));
+    loadSettings();
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+
+void MainWindow::loadSettings()
+{
+    QSettings settings("eyeProtector.ini", QSettings::IniFormat);
+    ui->m_lockTime->setText(settings.value("locktime",60).toString());
+    ui->m_cbAutoStart->setChecked(settings.value("autostart",true).toBool());
+}
+
+
+void MainWindow::saveSettings()
+{
+
+    QSettings settings("eyeProtector.ini", QSettings::IniFormat);
+    settings.setValue("locktime", ui->m_lockTime->text().toInt());
+    settings.setValue("autostart", ui->m_cbAutoStart->isChecked());
+
+}
+
+
+void MainWindow::on_m_saveBtn_clicked()
+{
+    saveSettings();
 }
--- mainwindow.h
+++ mainwindow.h
@@ -15,8 +15,16 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+private slots:
+    void on_m_saveBtn_clicked();
+
 private:
     Ui::MainWindow *ui;
+
+
+private:
+    void loadSettings();
+    void saveSettings();
 };
 
 #endif // MAINWINDOW_H
